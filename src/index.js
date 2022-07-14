7adfc1ecf43b5c2ebbbaeb19a82b845f0668ef1b
manyll90 <garethmanyll@gmail.com>
2022-07-14 17:28:02 +0300

    feet:add reverse number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n) + ""
+    let reverseStr = "";
+    for (let i = 0; i < n.length; i ++) {
+      reverseStr = n[i] + reverseStr;
+      /*console.log(reverseStr);*/
+    }
+    return Number(reverseStr);
 }
